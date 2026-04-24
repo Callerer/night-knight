785ccbcee73888c3108f46d78c52812357770104
Callerer <callererserror@proton.me>
2026-04-24 16:11:33 +0530

    controls update

--- scripts/player.gd
+++ scripts/player.gd
@@ -13,15 +13,15 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and (is_on_floor() || !coyote_timer.is_stopped()):
+	if Input.is_action_just_pressed("jump") and (is_on_floor() || !coyote_timer.is_stopped()):
 		velocity.y = JUMP_VELOCITY
 	elif velocity.y < 0.0:
-		if Input.is_action_just_released("ui_accept"):
+		if Input.is_action_just_released("jump"):
 			velocity.y *= 0.4
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	
 	if direction:
 		# We use move_toward for basic acceleration/deceleration
